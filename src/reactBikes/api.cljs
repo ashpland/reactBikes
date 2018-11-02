341f1b2081d9064c92be9ce73636c8298f2e78ee
Andrew Shopland <hi@ashp.land>
2018-11-02 01:35:38 -0700

    JSON parses to station

--- src/reactBikes/api.cljs
+++ src/reactBikes/api.cljs
@@ -1,16 +1,34 @@
 (ns reactBikes.api
-  (:require [reactBikes.state :as state]))
+  (:require [reactBikes.state :as state]
+            [clojure.string :as str]))
 
 (def testJSON "{\"name\":\"0001 10th & Cambie\",\"coordinates\":\"49.262487, -123.114397\",\"total_slots\":52,\"free_slots\":32,\"avl_bikes\":20,\"operative\":true,\"style\":\"\"}")
 
 (def output (js->clj (.parse js/JSON testJSON)))
 
+(defn splitID
+  [nameString]
+  (if (string? nameString)
+    (let [components (str/split nameString #" " 2)]
+      {:id (keyword (components 0)) :stationName (components 1)})))
+
+(defn splitCoordinates
+  [coordinatesString]
+  (if (string? coordinatesString)
+    (let [components (str/split coordinatesString #", " 2)]
+      {:coordinates {:lat (js/parseFloat (components 0))
+                     :lon (js/parseFloat (components 1))}})))
+
+(defn parseStation
+  [rawStation]
+  (merge (splitID (get rawStation "name"))
+         (splitCoordinates (get rawStation "coordinates"))
+         {:availableDocks (get rawStation "free_slots")
+          :availableBikes (get rawStation "avl_bikes")}))
+
+(def newStation (parseStation output))
+
 (defn convertJSON
   []
-  (swap! state/stations assoc
-         :0002 {:id :0002
-                :stationName "Thsdfioj Station"
-                :coordinates {:lat 0.0 :lon 0.0}
-                :availableDocks (get output "free_slots")
-                :availableBikes (get output "avl_bikes")})
+  (swap! state/stations assoc (newStation :id) newStation)
   [:div])
