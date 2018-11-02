efe6d16cb19fe4a94c705c47005482787686b22c
Andrew Shopland <hi@ashp.land>
2018-11-02 00:54:57 -0700

    Updates state

--- src/reactBikes/api.cljs
+++ src/reactBikes/api.cljs
@@ -1,7 +1,16 @@
-(ns reactBikes.api)
+(ns reactBikes.api
+  (:require [reactBikes.state :as state]))
 
 (def testJSON "{\"name\":\"0001 10th & Cambie\",\"coordinates\":\"49.262487, -123.114397\",\"total_slots\":52,\"free_slots\":32,\"avl_bikes\":20,\"operative\":true,\"style\":\"\"}")
 
+(def output (js->clj (.parse js/JSON testJSON)))
+
 (defn convertJSON
   []
-  [:div.test (get (js->clj (.parse js/JSON testJSON)) "name")])
+  (swap! state/stations assoc
+         :0002 {:id :0002
+                :stationName "Thsdfioj Station"
+                :coordinates {:lat 0.0 :lon 0.0}
+                :availableDocks (get output "free_slots")
+                :availableBikes (get output "avl_bikes")})
+  [:div])
